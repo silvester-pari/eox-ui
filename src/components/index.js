9824e464b027b9e950b41a37c512f361efba50df
paris <silvester.pari@eox.at>
2019-04-04 13:48:00 +0200

    added missing import in index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,12 +2,14 @@ import Vue from "vue";
 import EoxLogo from "./EoxLogo.vue";
 import EoxNavbar from "./EoxNavbar.vue";
 import EoxPageContent from "./EoxPageContent.vue";
+import EoxPageLayout from "./EoxPageLayout.vue";
 import EoxFooter from "./EoxFooter.vue";
 
 const Components = {
     EoxLogo,
     EoxNavbar,
     EoxPageContent,
+    EoxPageLayout,
     EoxFooter
 }
 
