26e3400c446ed42c516d982ad42837a6291b896c
paris <silvester.pari@eox.at>
2019-04-05 15:15:54 +0200

    temporary fix for the navbar logo

--- src/components/EoxLogo.vue
+++ src/components/EoxLogo.vue
@@ -1,16 +1,17 @@
 <template>
-  <img :src="logoSrc" height="100%">
+  <!-- <img :src="logoSrc" height="100%"> -->
+  <img src="https://eox.at/wp-content/uploads/2017/09/EOX_Logo.svg" height="100%">
 </template>
 
 <script>
-import logo from "@/assets/EOX_Logo.svg";
+// import logo from "@/assets/EOX_Logo.svg"; TEMP
 
 export default {
-  data() {
-    return {
-      logoSrc: logo
-    };
-  }
+  // data() {
+  //   return {
+  //     logoSrc: logo
+  //   };
+  // }
 };
 </script>
 
